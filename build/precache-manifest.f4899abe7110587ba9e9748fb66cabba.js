06e5174749be76067fa1e3ef601c60e47b82cf4f
Max <ferromassimo1989@gmail.com>
2020-07-05 16:43:44 +0200

    v1

---
+++ build/precache-manifest.f4899abe7110587ba9e9748fb66cabba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e837610f6e506c062cdea15e2e4fb422",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33c97b0d5fd5a6fb7455",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8b6822c5385225f8fdde",
+    "url": "/static/css/main.d0ce3252.chunk.css"
+  },
+  {
+    "revision": "33c97b0d5fd5a6fb7455",
+    "url": "/static/js/2.915a4bcb.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.915a4bcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b6822c5385225f8fdde",
+    "url": "/static/js/main.f04fbab0.chunk.js"
+  },
+  {
+    "revision": "87b11cb8729c839a2f80",
+    "url": "/static/js/runtime-main.7eaf3a3c.js"
+  }
+]);
